c55a0b5317281ee7a6fff76b2558d35d46298655
岡原加奈 <kokahara@okaharakananoMacBook-Air.local>
2021-04-02 16:39:30 +0900

    up to 4-5

--- wordpressnote/index.php
+++ wordpressnote/index.php
@@ -5,8 +5,24 @@
     <meta name="viewport" content="width=device-width">
     <?php wp_head(); ?>
   </head>
+
   <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
+
+    <header>
+    </header>
+
+    <nav>
+    </nav>
+
+    <main>
+    </main>
+
+    <aside>
+    </aside>
+
+    <footer>
+    </footer>
 
     <?php wp_footer(); ?>
   </body>
