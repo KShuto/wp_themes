bf8590fa01ce46afc6bba6c58a71390035bab898
岡原加奈 <kokahara@okaharakananoMacBook-Air.local>
2021-04-02 17:20:04 +0900

    up to 4-6

--- wordpressnote/function.php
+++ wordpressnote/function.php
@@ -2,9 +2,14 @@
 
 // 基本設定
 function wordpressnote_setup() {
+
   // ページのタイトルを出力
   add_theme_support( 'title-tag' );
   // HTML5対応
   add_theme_support( 'html5', array( 'style', 'script' ) );
+  // アイキャッチ画像
+  add_theme_support( 'post-thumbnails' );
+
 }
+
 add_action( 'after_setup_theme', 'wordpressnote_setup' );
--- wordpressnote/index.php
+++ wordpressnote/index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width">
+
     <?php wp_head(); ?>
   </head>
 
@@ -16,6 +17,24 @@
     </nav>
 
     <main>
+      <?php if(have_posts()):
+        while(have_posts()):
+          the_post(); ?>
+          <!-- 記事のひとまとまりを明示 -->
+          <article <?php post_class(); ?>>
+            <!-- アイキャッチ画像の指定がある場合は出力 -->
+            <?php if( has_post_thumbnail() ): ?>
+              <figure>
+                <?php the_post_thumbnail(); ?>
+              </figure>
+            <?php endif; ?>
+            <!-- タイトル表示 -->
+            <h1><?php the_title(); ?></h1>
+            <!-- コンテンツ表示 -->
+            <?php the_content(); ?>
+          </article>
+        <?php endwhile;
+      endif; ?>
     </main>
 
     <aside>
