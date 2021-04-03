75e69bfbad67eb983977f59ebc3193a6d3ee9be6
kokahara <kshutopen@gmail.com>
2021-04-03 21:50:36 +0900

    up to 5-6

--- wordpressnote/functions.php
+++ wordpressnote/functions.php
@@ -30,7 +30,7 @@ function wordpressnote_widgets() {
   ) );
 }
 
-add_action( 'widgets_init', 'wordpressnote_widgets');
+add_action( 'widgets_init', 'wordpressnote_widgets' );
 
 // CSS
 function wordpressnote_enqueue() {
@@ -42,4 +42,4 @@ function wordpressnote_enqueue() {
   wp_enqueue_style( 'wordpressnote-style', get_stylesheet_uri(), array(), filemtime(  get_template_directory() . '/style.css' ) );
 }
 
-add_action( 'wp_enqueue_scripts', 'wordpressnote_enqueue');
+  add_action( 'wp_enqueue_scripts', 'wordpressnote_enqueue' );
